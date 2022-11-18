eb8ee7f0d1b0cb8040fb966a4ecad654beadbeab
Nikolay Stoyanov <nikitig1987@abv.bg>
2022-11-18 15:45:12 +0100

    Add project files.

---
+++ T03.SumPrimeNonPrime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T03.SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int num;
+
+            int prineSum = 0;
+            int nonPrimeSum = 0;
+            bool isPrime;
+
+            while (input != "stop")
+            {
+                num = int.Parse(input);
+                isPrime = true;
+                if (num < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    for (int i = 2; i < num; i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            isPrime = false;
+                            nonPrimeSum = nonPrimeSum + num;
+                            break;
+                        }
+                    }
+                    if (isPrime == true)
+                    {
+                        prineSum = prineSum + num;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {prineSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+    }
+}
---
+++ Т04.TrainTheTrainers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Т04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string presentation = Console.ReadLine();
+            int students = 0;
+            double evaluation = 0;
+            double personalEvaluation;
+
+            while (presentation != "Finish")
+            {
+                personalEvaluation = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    personalEvaluation += double.Parse(Console.ReadLine());
+                }
+                personalEvaluation = personalEvaluation / n;
+                Console.WriteLine($"{presentation} - {personalEvaluation:F2}.");
+                students++;
+
+                evaluation += personalEvaluation;
+                presentation = Console.ReadLine();
+            }
+            evaluation = evaluation / students;
+            Console.WriteLine($"Student's final assessment is {evaluation:F2}.");
+        }
+    }
+}
